d42108fef2b47d61bd679745738cb272869787f3
Nate Sherfick <natesherfick@gmail.com>
2020-04-13 22:10:30 -0600

    looping completed

--- src/components/LoopingExercise.vue
+++ src/components/LoopingExercise.vue
@@ -9,23 +9,23 @@
                         object to put the value into our html.</p>
                     <p>Then use the index to print the players place in the collection.</p>
                     <!-- add the v-for to player-card -->
-                    <div class="player-card text-center">
-                        <h5> {{
-                            <!--code here--> }}</h5>
+                    <div class="player-card text-center" v-for='(player, index) in players'>
+                        <h5>
+                            Player {{ index + 1 }}</h5>
                         <div>
-                            <img class="img" src="" />
+                            <img class="img" :src="player.photo" />
                         </div>
                         <div>
                             <span>{{
-                                <!--code here-->}}</span>
+                                player.name}}</span>
                         </div>
                         <div>
                             <span>{{
-                                <!--code here--> }}</span>
+                                player.position}}</span>
                         </div>
                         <div>
                             <span>{{
-                                <!--code here--> }}</span>
+                                player.number}}</span>
                         </div>
                     </div>
                 </div>
@@ -36,8 +36,8 @@
                         and value contained at that key.</p>
                     <div class="blog" v-for="(value, key) in blog">
                         <p>{{
-                            <!--code here-->}}: {{
-                            <!--code here-->}}</p>
+                            key}}: {{
+                            value}}</p>
                     </div>
                 </div>
             </div>
